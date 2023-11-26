f60f0e5cfd28e9f53f1ec74e31f09e3c3b6317a2
Joe Barrow <joseph.d.barrow@gmail.com>
2023-11-25 21:39:34 -0500

    FullNNIndex tests

--- tests/test_knn.py
+++ tests/test_knn.py
@@ -1,4 +1,5 @@
 from tinyhnsw.knn import _cosine_similarity
+from tinyhnsw import FullNNIndex
 
 import numpy
 
@@ -9,3 +10,35 @@ def test_cosine_similarity():
     # test that self-similarity results in 1.
     self_sim = _cosine_similarity(X, X)
     assert numpy.allclose(numpy.diag(self_sim), numpy.ones(10))
+
+
+def test_add():
+    X = numpy.random.randn(10, 100)
+    index = FullNNIndex(100)
+    index.add(X)
+    assert index.ntotal == 10
+    index.add(X)
+    assert index.ntotal == 20
+
+
+def test_search_shapes():
+    X = numpy.random.randn(10, 100)
+    index = FullNNIndex(100)
+    index.add(X)
+    D, I = index.search(X, 5)
+
+    assert D.shape == (10, 5)
+    assert I.shape == (10, 5)
+
+
+def test_self_search():
+    X = numpy.random.randn(10, 100)
+    index = FullNNIndex(100)
+    index.add(X)
+    D, I = index.search(X, 1)
+
+    assert D.shape == (10, 1)
+    assert I.shape == (10, 1)
+
+    assert numpy.allclose(D[:, 0], numpy.ones_like(D[:, 0]))
+    assert numpy.allclose(I, numpy.expand_dims(numpy.arange(10), axis=1))

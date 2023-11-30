b126bc502ccbc0a1e9f78b39d7fddd28aa90766e
Joe Barrow <joseph.d.barrow@gmail.com>
2023-11-29 22:12:55 -0500

    Tests go green

--- tests/test_knn.py
+++ tests/test_knn.py
@@ -1,4 +1,4 @@
-from tinyhnsw.knn import _cosine_similarity
+from tinyhnsw.knn import cosine_similarity
 from tinyhnsw import FullNNIndex
 
 import numpy
@@ -8,7 +8,7 @@ def test_cosine_similarity():
     # X is a matrix of 10 vectors with dimension 100
     X = numpy.random.randn(10, 100)
     # test that self-similarity results in 1.
-    self_sim = _cosine_similarity(X, X)
+    self_sim = cosine_similarity(X, X)
     assert numpy.allclose(numpy.diag(self_sim), numpy.ones(10))
 
 

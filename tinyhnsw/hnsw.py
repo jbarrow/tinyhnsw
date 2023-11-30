ef2b81ddab3ca5bf554a68a442982af6734f60db
Joe Barrow <joseph.d.barrow@gmail.com>
2023-11-29 20:39:56 -0500

    Almost there

--- tinyhnsw/hnsw.py
+++ tinyhnsw/hnsw.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 from tinyhnsw.index import Index
-from tinyhnsw.knn import FullNNIndex, cosine_similarity
-from matplotlib.patches import Rectangle
+from tinyhnsw.knn import cosine_similarity
+from dataclasses import dataclass
+from heapq import nlargest, nsmallest, heappop, heappush
 
 import numpy
 import math
@@ -10,27 +11,34 @@ import networkx
 import matplotlib.pyplot as plt
 
 
+@dataclass
+class HNSWConfig:
+    M: int
+    M_max: int
+    M_max0: int
+    m_L: float
+    ef_construction: int
+
+
+DEFAULT_CONFIG = HNSWConfig(
+    M=3, M_max=3, M_max0=6, m_L=(1.0 / math.log(3)), ef_construction=32
+)
+
+
 class HNSWIndex(Index):
-    def __init__(
-        self, d: int, M: int, ef_construction: int = 32, ef_search: int = 32
-    ) -> None:
+    def __init__(self, d: int, config: HNSWConfig = DEFAULT_CONFIG) -> None:
         super().__init__(d)
 
-        self.M = M
-        self.M_max = M
-        self.M_max0 = 2 * M
-
-        self.ef_construction = ef_construction
-        self.ef_search = ef_search
+        self.config = config
         self.vectors = None
 
-        # m_L
-        self.m_L = 1.0 / math.log(M)
+        self.layers = [HNSWLayer(self, 0)]
+        self.ep = 0
+        self.L = 0
+        self.ix = 0
 
-        self.graphs = []
-        self.entry_point = None
-        self.L = -1
-        self.current_ix = 0
+    def assign_level(self) -> int:
+        return math.floor(-math.log(random.random()) * self.config.m_L)
 
     def add(self, vectors: numpy.ndarray) -> None:
         assert vectors.shape[1] == self.d
@@ -46,140 +54,103 @@ class HNSWIndex(Index):
         for vector in vectors:
             self.insert_into_graph(vector)
 
+    def insert_into_graph(self, q: numpy.ndarray):
+        l = self.assign_level()
+        L = self.L
+        ep = self.ep
+        ix = self.ix
+
+        for layer in range(L, l, -1):
+            _, W = self.layers[layer].search(q, ep, ef=1)
+            ep = W[0]
+
+        for layer in range(min(L, l), -1, -1):
+            self.layers[layer].insert(q, ix, ep)
+
+        if l > self.L:
+            for l_new in range(L + 1, l + 1):
+                self.layers.append(HNSWLayer(self, l_new, self.ix))
+            self.L = L
+            self.ep = ix
+
+        self.ix += 1
+
     def distance(self, q, v):
-        c = cosine_similarity(numpy.expand_dims(q, axis=0), numpy.expand_dims(v, axis=0))
-        return c
+        if len(q.shape) == 1:
+            q = numpy.expand_dims(q, axis=0)
 
-    def insert_into_graph(self, vector: numpy.ndarray) -> None:
-        level = self.assign_level()
+        if len(v.shape) == 1:
+            v = numpy.expand_dims(v, axis=0)
 
-        if level > self.L:
-            for i in range(self.L + 1, level + 1):
-                G = networkx.Graph()
-                self.graphs.append(G)
-            self.L = level
+        return cosine_similarity(q, v)
 
-        # Initialize the node in all layers up to its level
-        for i in range(level + 1):
-            if self.current_ix not in self.graphs[i]:
-                self.graphs[i].add_node(self.current_ix)
 
-        # Set the entry point if this is the highest level node so far
-        if level == self.L:
-            self.entry_point = self.current_ix
+class HNSWLayer:
+    def __init__(self, index: HNSWIndex, lc: int, ep: int | None = None) -> None:
+        self.G = networkx.Graph()
+        self.index = index
+        self.config = self.index.config
 
-        ep = self.entry_point
+        if ep is not None:
+            self.G.add_node(ep)
 
-        # Start from the top layer and work down to layer 0
-        for lc in range(self.L, -1, -1):
-            if lc > level:
-                continue
+        if lc == 0:
+            self.M_max = self.config.M_max0
+        else:
+            self.M_max = self.config.M_max
 
-            # Search for the ef closest elements in the current layer
-            W = self.search_layer(vector, ep, ef=self.ef_construction, layer=lc)
-            neighbors = self.select_neighbors(vector, W, self.M, lc)
+    def distance_to_node(self, q: numpy.ndarray, e: int) -> float:
+        v = self.index.vectors[e]
+        return self.index.distance(q, v)[0]
 
-            # Add edges between the current node and its neighbors in the graph
-            for neighbor in neighbors:
-                self.graphs[lc].add_edge(self.current_ix, neighbor)
-
-                # Check if the neighbor has too many connections and prune if necessary
-                eConn = list(self.graphs[lc][neighbor])
-                if len(eConn) > self.M_max:
-                    if lc == 0:
-                        Mmax = self.M_max0
-                    else:
-                        Mmax = self.M_max
-                    eNewConn = self.select_neighbors(self.vectors[neighbor], eConn, Mmax, lc)
-                    # Update the neighbor's connections
-                    self.graphs[lc].remove_node(neighbor)
-                    self.graphs[lc].add_node(neighbor)
-                    for new_neighbor in eNewConn:
-                        self.graphs[lc].add_edge(neighbor, new_neighbor)
-
-            # Update the entry point for the next lower layer
-            if len(W) > 0:
-                ep = min(W, key=lambda x: self.distance(vector, self.vectors[x]))
-
-        self.current_ix += 1
-
-    def search_layer(
-        self, vector: numpy.ndarray, entry_point: int, layer: int, ef: int = 1
+    def search(
+        self, q: numpy.ndarray, ep: int, ef: int
     ) -> tuple[list[float], list[int]]:
-        visited = set([self.entry_point])
-        candidates = {
-            self.entry_point: self.distance(vector, self.vectors[entry_point].squeeze())
-        }
-        neighbors = {
-            self.entry_point: self.distance(vector, self.vectors[entry_point].squeeze())
-        }
+        ep_dist = self.distance_to_node(q, ep)
 
-        while len(candidates) > 0:
-            c, c_distance = min(candidates.items(), key=lambda x: x[1])
-            f, f_distance = max(neighbors.items(), key=lambda x: x[1])
+        v = {ep}
+        C = [(ep_dist, ep)]
+        W = [(ep_dist, ep)]
 
-            if c_distance > f_distance:
+        while len(C) > 0:
+            d_c, c = heappop(C)
+            d_f, f = nlargest(1, W, key=lambda x: x[0])[0]
+
+            if d_c > d_f:
                 break
 
-            for e in self.graphs[layer][c]:
-                if e not in visited:
-                    visited.add(e)
-                    e_distance = self.distance(vector, self.vectors[e])
+            for e in self.G[c]:
+                if e not in v:
+                    v.add(e)
+                    d_f, f = nlargest(1, W, key=lambda x: x[0])[0]
+                    d_e = self.distance_to_node(q, e)
 
-                    f, f_distance = max(neighbors.items(), key=lambda x: x[1])
-                    if e_distance < f_distance or len(neighbors) < ef:
-                        candidates[e] = e_distance
-                        neighbors[e] = e_distance
+                    if d_e < d_f or len(W) < ef:
+                        heappush(C, (d_e, e))
+                        heappush(W, (d_e, e))
 
-                        if len(neighbors) > ef:
-                            furthest = max(neighbors, key=neighbors.get)
-                            del neighbors[furthest]
+                        if len(W) > ef:
+                            W = nsmallest(ef, W, key=lambda x: x[0])
 
-            del candidates[c]
-        print(neighbors)
-        return list(neighbors.keys())
+        return zip(*W)
+
+    def insert(self, q: numpy.ndarray, node: int, ep: int) -> None:
+        if len(self.G) == 0:
+            self.G.add_node(node)
+            return
+        
+        D, W = self.search(q, ep, self.config.ef_construction)
+        neighbors = self.select_neighbors(q, D, W)
+        self.G.add_edges_from([(e, node) for e in neighbors])
+
+        # for e in neighbors:
+        #     if len(self.G[e]) > self.M_max:
+        #         eConn = self.select_neighbors()
 
     def select_neighbors(
-        self,
-        q: numpy.ndarray,
-        C: set,
-        M: int,
-        lc: int,
-        extendCandidates: bool = False,
-        keepPrunedConnections: bool = False,
-    ) -> set:
-        R = set()
-        W = C.copy()
-
-        if extendCandidates:
-            for e in C:
-                for e_adj in self.graphs[lc][e]:
-                    if e_adj not in W:
-                        W.add(e_adj)
-
-        W_d = set()
-
-        while len(W) > 0 and len(R) < M:
-            e = min(W, key=lambda x: self.distance(q, self.vectors[x]))
-            W.remove(e)
-
-            if len(R) == 0 or self.distance(q, self.vectors[e]) < max(
-                [self.distance(q, self.vectors[r]) for r in R]
-            ):
-                R.add(e)
-            else:
-                W_d.add(e)
-
-        if keepPrunedConnections:
-            while len(W_d) > 0 and len(R) < M:
-                e = min(W_d, key=lambda x: self.distance(q, self.vectors[x]))
-                W_d.remove(e)
-                R.add(e)
-
-        return R
-
-    def assign_level(self) -> int:
-        return math.floor(-math.log(random.random()) * self.m_L)
+        self, q: numpy.ndarray, D: list[float], W: list[int]
+    ) -> list[int]:
+        return W[: self.config.M]
 
 
 def visualize_hnsw_index(index: HNSWIndex):
@@ -188,39 +159,28 @@ def visualize_hnsw_index(index: HNSWIndex):
     maintain consistent locations between layers, and the layers are
     plotted next to each other.
     """
-    _, axs = plt.subplots(1, len(index.graphs), figsize=(len(index.graphs) * 5, 5))
 
-    layout = networkx.spring_layout(index.graphs[0])
-    node_color = [
-        "r" if index.entry_point == node else "c" for node, _ in layout.items()
-    ]
+    _, axs = plt.subplots(1, len(index.layers), figsize=(len(index.layers) * 5, 5))
 
-    for i, graph in enumerate(index.graphs):
+    layout = networkx.spring_layout(index.layers[0].G)
+    node_color = ["r" if index.ep == node else "c" for node, _ in layout.items()]
+
+    for i, layer in enumerate(index.layers):
+        graph = layer.G
+
         graph_layout = {k: v for k, v in layout.items() if k in graph}
         graph_node_color = [node_color[k] for k, _ in graph_layout.items()]
         networkx.draw(
             graph, graph_layout, ax=axs[i], node_size=25, node_color=graph_node_color
         )
-        axs[i].set_title(f"Layer {i}")
-
-        # Add a border around each subplot
-        rect = Rectangle(
-            (0, 0),
-            1,
-            1,
-            linewidth=2,
-            edgecolor="black",
-            facecolor="none",
-            transform=axs[i].transAxes,
-        )
-        axs[i].add_patch(rect)
+        # axs[i].set_title(f"Layer {i}")
 
     plt.show()
 
 
 if __name__ == "__main__":
-    index = HNSWIndex(10, 3)
-    vectors = numpy.random.randn(10, 10)
+    index = HNSWIndex(2)
+    vectors = numpy.random.randn(10, 2)
     index.add(vectors)
 
     visualize_hnsw_index(index)
